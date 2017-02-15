167bf024b6ff7b333afc4d2cb78709d37045f530
Evan Su <evansu@nxnode-1.cse.unr.edu>
2017-02-15 12:31:47 -0800

    v0.2 added iteration to pingpong.c

--- PA01/src/mpi_pingpong.c
+++ PA01/src/mpi_pingpong.c
@@ -1,9 +1,9 @@
 /******************************************************************************
-* FILE: mpi_hello.c
+* FILE: mpi_pingpong.c
 * DESCRIPTION:
-*   MPI tutorial example code: Simple hello world program
-* AUTHOR: Blaise Barney
-* LAST REVISED: 03/05/10
+*   MPI tutorial example code: Simple pingpong program
+* AUTHOR: Evan Su
+* LAST REVISED: 02/15/17
 ******************************************************************************/
 #include "mpi.h"
 #include <sys/time.h>
@@ -17,7 +17,8 @@ int main (int argc, char *argv[])
 int   numtasks, taskid, len;
 char hostname[MPI_MAX_PROCESSOR_NAME];
 int number;
-int iteration = 0;
+int iteration = 50;
+int index;
 float averageTime = 0;
 struct timeval startTime, endTime, diffTime, totalTime;
 //preprocessing
@@ -36,23 +37,30 @@ if (taskid == MASTER)
 {
 	number = -1;
 	gettimeofday(&startTime, NULL); //start clock
-	MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-	//printf("finish send\n");
-	MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	gettimeofday(&endTime, NULL); //end clock
+	for(index = 0; index < iteration ; index++)
+	{
+		MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		//printf("finish send\n");
+		MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		gettimeofday(&endTime, NULL); //end clock
 
-	timersub(&endTime, &startTime, &diffTime); //calc diff time
-	timeradd(&diffTime, &totalTime, &totalTime); //acculmate diff time
-	iteration += 2;//increment number of iterations
-	averageTime = (totalTime.tv_sec * 1000000 + totalTime.tv_usec)/iteration;
+		timersub(&endTime, &startTime, &diffTime); //calc diff time
+		timeradd(&diffTime, &totalTime, &totalTime); //acculmate diff time
+
+	}
+	averageTime = (totalTime.tv_sec * 1000000 + totalTime.tv_usec)/ (iteration*2);
+	printf("total iterations:%d",iteration *2);
 	printf("average travel time %f microseconds\n", averageTime); //average printout
 	//printf("got reply\n");
 }
 else
 {
-	MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	//printf("recieved packet\n");
-	MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	for(index = 0; index < iteration ; index++)
+	{
+		MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		//printf("recieved packet\n");
+		MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
 }
 MPI_Finalize();
 printf("Task done\n");
