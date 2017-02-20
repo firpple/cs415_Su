7a8cd46458ecfa72c480b4c210b07f3af3ae33a5
Evan Su <evansu@nevada.unr.edu>
2017-02-19 18:26:42 -0800

    v0.3 cleaned up code

--- PA01/src/mpi_packetCheck.c
+++ PA01/src/mpi_packetCheck.c
@@ -1,7 +1,9 @@
 /******************************************************************************
 * FILE: mpi_packetCheck.c
 * DESCRIPTION:
-*   MPI tutorial example code: Simple pingpong program
+*   MPI Send and recieved. The program outputs the ellapsed time between mpi 
+*   send and mpi receive for increasing message sizes. The ellapsed time is
+*   measured in microseconds
 * AUTHOR: Evan Su
 * LAST REVISED: 02/15/17
 ******************************************************************************/
@@ -11,70 +13,76 @@
 #include <stdlib.h>
 #define  MASTER		0
 #define  SLAVE      1
-#define ARRAYSIZE   10000
-#define ITERATIONS  50
+#define  TAG        0
+#define  ARRAYSIZE  10000
+#define  ITERATIONS 50
+#define  SECTOMICRO 1000000
+
 
 int main (int argc, char *argv[])
 {
-int   numtasks, taskid, len;
-char hostname[MPI_MAX_PROCESSOR_NAME];
-int number;
-int numberArray[ARRAYSIZE];
-int index,indexS;
-float averageTime = 0;
-float elapsedTime = 0;
-struct timeval startTime, endTime, diffTime, totalTime;
-//preprocessing
-timerclear(&totalTime);
-timerclear(&diffTime);
+    //preprocessing
+    int numtasks, taskid, len;
+    int numberArray[ARRAYSIZE];
+    int index,indexS;
+    char hostname[MPI_MAX_PROCESSOR_NAME];
+    float elapsedTime = 0;
+    struct timeval startTime, endTime, diffTime;
+    timerclear(&diffTime);
 
-//paralle start
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
-MPI_Get_processor_name(hostname, &len);
+    //parallel start
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+    MPI_Get_processor_name(hostname, &len);
 
+    //the printf here is only used for debugging
+    //printf ("Hello from task %d on %s!\n", taskid, hostname);
 
-//printf ("Hello from task %d on %s!\n", taskid, hostname);
-for(indexS = 0; indexS < ARRAYSIZE; indexS++)
-{
-	timerclear(&totalTime);
-	timerclear(&diffTime);
+    //Iterator indexs is for message size
+    for(indexS = 0; indexS < ARRAYSIZE; indexS++)
+    {
+	    timerclear(&diffTime);
 
-	if (taskid == MASTER)
-	{
-		
-		for(index = 0; index < ITERATIONS ; index++)
-		{
-			gettimeofday(&startTime, NULL); //start clock
-			MPI_Send(numberArray, indexS, MPI_INT, 1, 0, MPI_COMM_WORLD);
-			MPI_Recv(numberArray, indexS, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			gettimeofday(&endTime, NULL); //end clock
+	    if (taskid == MASTER)
+	    {
+		    //master code
+            //Iterator index is for sending the same size message multiple times.
+		    for(index = 0; index < ITERATIONS ; index++)
+		    {
+			    gettimeofday(&startTime, NULL); //start clock
+                //send to slave
+			    MPI_Send(numberArray, indexS, MPI_INT, SLAVE, TAG, MPI_COMM_WORLD);
+                //recieve from slave
+			    MPI_Recv(numberArray, indexS, MPI_INT, SLAVE, TAG, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+			    gettimeofday(&endTime, NULL); //end clock
 
-			timersub(&endTime, &startTime, &diffTime); //calc diff time
-			timeradd(&diffTime, &totalTime, &totalTime); //acculmate diff time
-			elapsedTime = (diffTime.tv_sec * 1000000 + diffTime.tv_usec);
-			printf("%f,",elapsedTime );
+			    timersub(&endTime, &startTime, &diffTime); //calc diff time
+                //converts time struct to float
+			    elapsedTime = (diffTime.tv_sec * SECTOMICRO + diffTime.tv_usec); 
+                //prints result in CSV format
+			    printf("%f,",elapsedTime );
 
-		}
-		printf("\n");
-		//averageTime = (totalTime.tv_sec * 1000000 + totalTime.tv_usec)/ (ITERATIONS*2);
-		//averageTime = (totalTime.tv_sec * 1000000 + totalTime.tv_usec);
-		//printf("total ITERATIONS:%d",ITERATIONS *2);
-		//printf("Time: %10.0f microseconds for %d\n", averageTime, indexS); //average printout
-	}
-	else
-	{
-		for(index = 0; index < ITERATIONS ; index++)
-		{
-			MPI_Recv(numberArray, indexS, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			MPI_Send(numberArray, indexS, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		}
-	}
+		    }
+		    printf("\n");
+	    }
+	    else
+	    {
+            //slave code
+            //Iterator index is for sending the same size message multiple times.
+		    for(index = 0; index < ITERATIONS ; index++)
+		    {
+                //send to slave
+			    MPI_Recv(numberArray, indexS, MPI_INT, MASTER, TAG, MPI_COMM_WORLD,
+                        MPI_STATUS_IGNORE);
+                //recieve from slave
+			    MPI_Send(numberArray, indexS, MPI_INT, MASTER, TAG, MPI_COMM_WORLD);
+		    }
+	    }
 }
 
 MPI_Finalize();
-printf("Task done\n");
 return 0;
 
 
--- PA01/src/mpi_pingpong.c
+++ PA01/src/mpi_pingpong.c
@@ -1,7 +1,9 @@
 /******************************************************************************
-* FILE: mpi_pingpong.c
+* FILE: mpi_packetCheck.c
 * DESCRIPTION:
-*   MPI tutorial example code: Simple pingpong program
+*   MPI Send and recieved. The program outputs the ellapsed time between mpi 
+*   send and mpi receive for a single message. The ellapsed time is
+*   measured in microseconds
 * AUTHOR: Evan Su
 * LAST REVISED: 02/15/17
 ******************************************************************************/
@@ -11,59 +13,73 @@
 #include <stdlib.h>
 #define  MASTER		0
 #define  SLAVE      1
+#define  TAG        0
+#define  ARRAYSIZE  10000
+#define  ITERATIONS 50
+#define  SECTOMICRO 1000000
+
 
 int main (int argc, char *argv[])
 {
-int   numtasks, taskid, len;
-char hostname[MPI_MAX_PROCESSOR_NAME];
-int number;
-int iteration = 50;
-int index;
-float averageTime = 0;
-struct timeval startTime, endTime, diffTime, totalTime;
-//preprocessing
-timerclear(&totalTime);
-timerclear(&diffTime);
+    //preprocessing
+    int numtasks, taskid, len;
+    int numberArray[ARRAYSIZE];
+    int index;
+    char hostname[MPI_MAX_PROCESSOR_NAME];
+    float elapsedTime = 0;
+    struct timeval startTime, endTime, diffTime;
+    timerclear(&diffTime);
 
-//paralle start
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
-MPI_Get_processor_name(hostname, &len);
+    //parallel start
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+    MPI_Get_processor_name(hostname, &len);
 
+    //the printf here is only used for debugging
+    //printf ("Hello from task %d on %s!\n", taskid, hostname);
 
-printf ("Hello from task %d on %s!\n", taskid, hostname);
-if (taskid == MASTER)
-{
+    timerclear(&diffTime);
+
+    if (taskid == MASTER)
+    {
+	    //master code
+        //Iterator index is for sending the same size message multiple times.
+	    for(index = 0; index < ITERATIONS ; index++)
+	    {
+		    gettimeofday(&startTime, NULL); //start clock
+            //send to slave
+		    MPI_Send(numberArray, indexS, MPI_INT, SLAVE, TAG, MPI_COMM_WORLD);
+            //recieve from slave
+		    MPI_Recv(numberArray, indexS, MPI_INT, SLAVE, TAG, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+		    gettimeofday(&endTime, NULL); //end clock
+
+		    timersub(&endTime, &startTime, &diffTime); //calc diff time
+            //converts time struct to float
+		    elapsedTime = (diffTime.tv_sec * SECTOMICRO + diffTime.tv_usec); 
+            //prints result in CSV format
+		    printf("%f,",elapsedTime );
+
+	    }
+	    printf("\n");
+    }
+    else
+    {
+        //slave code
+        //Iterator index is for sending the same size message multiple times.
+	    for(index = 0; index < ITERATIONS ; index++)
+	    {
+            //send to slave
+		    MPI_Recv(numberArray, indexS, MPI_INT, MASTER, TAG, MPI_COMM_WORLD,
+                    MPI_STATUS_IGNORE);
+            //recieve from slave
+		    MPI_Send(numberArray, indexS, MPI_INT, MASTER, TAG, MPI_COMM_WORLD);
+	    }
 	
-	gettimeofday(&startTime, NULL); //start clock
-	for(index = 0; index < iteration ; index++)
-	{
-		MPI_Send(&number, 1, MPI_INT, SLAVE, 0, MPI_COMM_WORLD);
-		//printf("finish send\n");
-		MPI_Recv(&number, 1, MPI_INT, SLAVE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		gettimeofday(&endTime, NULL); //end clock
-
-		timersub(&endTime, &startTime, &diffTime); //calc diff time
-		timeradd(&diffTime, &totalTime, &totalTime); //acculmate diff time
-
-	}
-	averageTime = (totalTime.tv_sec * 1000000 + totalTime.tv_usec)/ (iteration*2);
-	printf("total iterations:%d",iteration *2);
-	printf("average travel time %f microseconds\n", averageTime); //average printout
-	//printf("got reply\n");
 }
-else
-{
-	for(index = 0; index < iteration ; index++)
-	{
-		MPI_Recv(&number, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		//printf("recieved packet\n");
-		MPI_Send(&number, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
-	}
-}
+
 MPI_Finalize();
-printf("Task done\n");
 return 0;
 
 
