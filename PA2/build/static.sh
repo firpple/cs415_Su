8ab4662d493b539d632ddd0796b12a088e550b08
Evan Su <evansu@nevada.unr.edu>
2017-03-12 22:54:27 -0700

    updated scripts after move

--- PA2/build/static.sh
+++ PA2/build/static.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#SBATCH --OverSubscribe = yes
 #SBATCH --nodes 1
 #SBATCH --ntasks 8
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=pikmin898@gmail.com
 #SBATCH --mail-type=ALL
 
-srun ~/cs415_Su/PA2/mpi_mandelStatic 5000 6000
+srun ~/cs415_Su/PA2/build/mpi_mandelStatic 5000 6000
