d06d0310ce633a4fc02b3258e572c1efa82b0972
Evan Su <evansu@nevada.unr.edu>
2017-04-03 21:59:13 -0700

    PAR v3.04

--- PA3/src/mpi_bucketPar.c
+++ PA3/src/mpi_bucketPar.c
@@ -248,7 +248,8 @@ void slaveCode(int buckets, char* fileName)
     
     for(indexOut = 0; indexOut < size; indexOut++ )
     {
-        //bucketIndex = unsortedArray[indexOut]/(MAXINT/ buckets);
+        bucketIndex = unsortedArray[indexOut]/(MAXINT/ buckets);
+        printf("%d ", bucketIndex);
         //nextIndex = smallBuckets[bucketIndex][0] + 1;
         //smallBuckets[bucketIndex][0]++;
         //smallBuckets[bucketIndex][nextIndex] = unsortedArray[indexOut]; 
