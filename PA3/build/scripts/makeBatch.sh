2bac26a701820247ab808a5cb0673ccff61cc728
Evan Su <evansu@nevada.unr.edu>
2017-04-03 21:00:26 -0700

    bash script updated

--- PA3/build/scripts/makeBatch.sh
+++ PA3/build/scripts/makeBatch.sh
@@ -19,3 +19,17 @@ EOF
 done
 
 done
+
+
+#parallel stuffs
+cat > SBBuckParB2S100 << EOF
+#!/bin/bash
+#SBATCH -o BSeqB2S100_%J.out
+#SBATCH --ntasks 2
+#SBATCH --nodes 1
+#SBATCH --time=00:05:00
+#SBATCH --mail-user=pikmin898@gmail.com
+#SBATCH --mail-type=ALL
+
+srun ~/cs415_Su/PA3/build/mpi_bucketParallel 2 ~/cs415_Su/PA3/build/unsorted/unsort_N100
+EOF
