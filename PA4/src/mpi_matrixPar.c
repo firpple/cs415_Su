ea42a61b435a601fc06021969763e0d925e6bf34
Evan Su <pikmin898@gmail.com>
2017-04-19 15:10:16 -0700

    Parallel v2.2

--- PA4/src/mpi_matrixPar.c
+++ PA4/src/mpi_matrixPar.c
@@ -441,7 +441,7 @@ void matrixInitCannon(int row,int col, int length, int matrixLength,
 	left = row *length + (row + length - 1) %length;
 	right = row *length + (row  + 1) %length;
 
-	//printf("me: %d, U:%d, D:%d, L:%d, R:%d\n", row*length +col, up, down, left, right);
+	printf("me: %d, U:%d, D:%d, L:%d, R:%d\n", row*length +col, up, down, left, right);
 	for(index = 0; index < row; index++)
 	{
 		rotateRow(left, right, length, matrixLength, sendBuffer, recvBuffer, matrixA);
