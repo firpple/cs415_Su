fda2ac1e7a7ac09ea8edd34b649ad856d9e8548b
Evan Su <pikmin898@gmail.com>
2017-04-19 12:52:00 -0700

    Parallel v2.1

--- PA4/src/mpi_matrixPar.c
+++ PA4/src/mpi_matrixPar.c
@@ -66,7 +66,7 @@ int main (int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
     MPI_Get_processor_name(hostname, &len);
 	
-	switch(taskid)
+	switch(numtask)
 	{
 		case 4:
 			length = 2;
